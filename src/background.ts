567c423ebcfa1f4a2f3375324ff0c993a6556a76
Jonas Walkling <jonas@walkling.email>
2023-02-12 11:11:38 +0100

    Check for videos all the time because coursware is a single page application and new videos aren't detected when switching to the next lecture otherwise

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,7 @@
-const getVideoArray = (): HTMLVideoElement[] =>
-  Array.from(document.getElementsByTagName("video"));
-
-const addControlls = () =>
-  getVideoArray().forEach((vid) => vid.setAttribute("controls", "true"));
-
-const setVideoInterval = setInterval(() => {
-  addControlls();
-  if (getVideoArray().length > 0) {
-    setTimeout(() => clearInterval(setVideoInterval), 1000);
-  }
-}, 100);
-
-//console.log("extension active");
+setInterval(
+  () =>
+    Array.from(document.getElementsByTagName("video")).forEach((vid) =>
+      vid.setAttribute("controls", "true")
+    ),
+  200
+);
